6af9cf10304facff2dc37077d4d882c9ac87d320
Salvador Gómez <salvawebdg@gmail.com>
2024-11-27 16:15:29 +0100

    Move current song effect to global state.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -8,21 +8,23 @@ export const Play = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 24 24"><path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path></svg>
 )
 export function Player () {
-    const { isPlaying, setIsPlaying } = usePlayerStore(state => state)
-    const [currentSong, setCurrentSong] = useState(null)
+    const { currentMusic, isPlaying, setIsPlaying } = usePlayerStore(state => state)
     const audioRef = useRef()
 
     useEffect(() => {
-        audioRef.current.src = `/music/1/01.mp3`
-    }, [])
+        isPlaying ? audioRef.current.play() : audioRef.current.pause()
+    }, [isPlaying])
+
+    useEffect(() => {
+        const { song, playlist, songs } = currentMusic
+        if (song) {
+            const src = `/music/${playlist?.id}/0${song.id}.mp3`
+            audioRef.current.src = src
+            audioRef.current.play()
+        }
+    }, [currentMusic])
 
     const handleClick = () => {
-        if (isPlaying) {
-            audioRef.current.pause()
-        } else {
-            audioRef.current.play()
-            audioRef.current.volume = 0.1
-        }
         setIsPlaying(!isPlaying)
     }
 
