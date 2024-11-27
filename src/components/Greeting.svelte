0f2341d101efe1959bd7baa02f0c3c184a4f0d36
Salvador Gómez <salvawebdg@gmail.com>
2024-11-27 13:09:08 +0100

    Add Greeting component.

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+    let greeting = ""
+
+    if (currentHour >= 20) {
+        greeting = "Buenas Noches"
+    } else if (currentHour >= 12) {
+        greeting = "Buenas tardes"
+    } else {
+        greeting = "Buenos días"
+    }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
